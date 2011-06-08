c78404d787f721a2677ce014329b8921f1c67a4e
clueless <clueless.nospam@gmail.com>
2011-06-08 18:23:08 +0200

    Changed (404) error handling to be based on exceptions allowing your applications to catch all unhandled exceptions easily

---
+++ htdocs/controller/error.php
@@ -0,0 +1,38 @@
+<?php
+    /**
+     * Basic example of an error controller
+     * Make sure that the error handling class does not itself throw exceptions
+     */
+    class Error implements ErrorInterface
+    {
+        private $exception;
+
+        public function __construct(FramelessException $e)
+        {
+            $this->exception =& $e;
+        }
+
+        public function Handle()
+        {
+           if($this->exception->GetCode() == ErrorCodes::E_404)
+                $this->Handle404();
+            else
+                $this->HandleUnknown();
+        }
+
+        private function Handle404()
+        {
+            header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+            echo '404 page not found';
+        }
+
+        private function HandleUnknown()
+        {
+            $msgs = $this->exception->GetAllMessages();
+            foreach($msgs as $m)
+            {
+                echo "$m<br />";
+            }
+        }
+    }
+?>
